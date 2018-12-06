95e3659434bbd3ef692b20233e629487258028d0
ksketo <kfaliagkas90@gmail.com>
2018-12-06 16:57:43 +0200

    Add basic design

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect("/wishlist");
 });
+
+router.get('/wishlist', function(req, res, next) {
+  res.render('index', {
+    title: 'Wishlist',
+    description: "Your Christmas Whishlist"
+  });
+});
+
+router.get('/shopping-list', function(req, res, next) {
+  res.render('index', {
+    title: 'Shopping List',
+    description: "Your Christmas Shopping List"
+  });
+});
+
+router.get('/gift-guides', function(req, res, next) {
+  res.render('index', {
+    title: 'Gift Guides',
+    description: "Christmas Gift Guides"
+  });
+});
+
 
 module.exports = router;
