5f567f0fe7bba9345864b0a16d16792d3c130d65
ksketo <kfaliagkas90@gmail.com>
2018-12-17 16:21:02 +0200

    Populate gift guide from server-rendered mock json file

--- routes/index.js
+++ routes/index.js
@@ -43,9 +43,12 @@ router.get('/shopping-list', function(req, res, next) {
 });
 
 router.get('/gift-guides', function(req, res, next) {
+  const guides = require("../public/data/gift-guides.json");
+
   res.render('gift-guides', {
     title: 'Gift Guides',
-    description: "Christmas Gift Guides"
+    description: "Christmas Gift Guides",
+    guides
   });
 });
 
